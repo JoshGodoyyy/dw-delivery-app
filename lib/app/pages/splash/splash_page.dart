8caf64137369b5e6cbb8453ffb7880696e671f89
Josh Godoy <josueaugusto@live.com>
2023-02-01 12:15:38 -0300

    Primeira aula Dart Week 9

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:delivery_app/app/core/config/env/env.dart';
+import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Splash')),
+      body: Column(
+        children: [
+          DeliveryButton(
+            label: Env.instance['backend_base_url'] ?? '',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
