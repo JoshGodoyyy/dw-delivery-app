9fac5e1fc26a1f82e1dcb56eb18eb8792315adfb
Josh Godoy <josueaugusto@live.com>
2023-02-02 09:51:57 -0300

    Segundo dia de aula DW9

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_app/app/core/config/env/env.dart';
+import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -8,14 +8,39 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Splash')),
-      body: Column(
-        children: [
-          DeliveryButton(
-            label: Env.instance['backend_base_url'] ?? '',
-            onPressed: () {},
-          ),
-        ],
+      body: ColoredBox(
+        color: const Color(0xFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: context.percentHeight(.3)),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 80),
+                  DeliveryButton(
+                    width: context.percentWidth(.6),
+                    height: 35,
+                    label: 'ACESSAR',
+                    onPressed: () {
+                      Navigator.of(context).popAndPushNamed('/home');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
